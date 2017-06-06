45cbb5a330a36055559388b54a2ec71a3b95fd72
patryk <pwitiw93@gmail.com>
2017-06-06 19:37:44 +0200

    logic

---
+++ Go4AMatch2/app/src/main/java/com/witiw/go4amatch/entities/GameType.java
@@ -0,0 +1,12 @@
+package com.witiw.go4amatch.entities;
+
+/**
+ * Created by Patryk on 04.06.2017.
+ */
+
+public enum GameType {
+
+    qualification,
+    group,
+    cup
+}
---
+++ Go4AMatch2/app/src/main/java/com/witiw/go4amatch/rest/api/sportradar/teaminfo/TeamProfile.java
@@ -0,0 +1,54 @@
+package com.witiw.go4amatch.rest.api.sportradar.teaminfo;
+
+import com.witiw.go4amatch.rest.api.sportradar.Team;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+/**
+ * Created by Patryk on 03.06.2017.
+ */
+@Root(name = "team_profile", strict = false)
+public class TeamProfile {
+
+    @Element
+    private Venue venue;
+
+    @Element
+    private Statistics statistics;
+
+    @Element(required = false)
+    private Team team;
+
+    // dodatkowe pole
+    private int rank;
+
+    public Statistics getStatistics() {
+        return statistics;
+    }
+
+    public void copy(TeamProfile teamProfile) {
+        venue = teamProfile.venue;
+        statistics = teamProfile.getStatistics();
+    }
+
+    public String getCity() {
+        return venue.getCityName();
+    }
+
+    public String getForm() {
+        return statistics.seasons.get(statistics.seasons.size() - 1).getForm().getTotal();
+    }
+
+    public String getTeamName() {
+        return team.getName();
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
